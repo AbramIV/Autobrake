aa499075c2fa322a8ebbe52ebad34968b525e801
AbramIV <Septrakon@gmail.com>
2024-01-08 23:43:26 -0600

    + Data Handler and Workers

---
+++ Software/DataHandler/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandler;
+
+internal class App
+{
+    internal App() { }
+
+    public void Run(string file)
+    {
+
+    }
+}
---
+++ Software/DataHandler/Program.cs
@@ -0,0 +1,7 @@
+﻿using DataHandler;
+
+var options = Environment.GetCommandLineArgs();
+
+if (options is null || options.Length < 2) Environment.Exit(-1);
+
+new App().Run(options[1]);
